88270fe3c2cc6c558f90d58d51f74725f07ebabb
Igor <igorbod.webdev@gmail.com>
2020-02-12 16:49:00 +0700

    made slider section with slick slider, also fix fonts

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  
+  $('.slider__inner').slick({
+    arrows: true,
+    prevArrow: '<button type="button" class="button__prev"><span class="icon-chevron-left"></span></button>',
+    nextArrow: '<button type="button" class="button__next"><span class="icon-chevron-right"></span></button>'
+  });
+
+});
