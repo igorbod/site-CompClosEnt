375a8514587fc370617b0e280934ea2a784a9c63
Igor <igorbod.webdev@gmail.com>
2020-02-14 11:33:11 +0700

    made section News with slider

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@ $(document).ready(function () {
     nextArrow: '<button type="button" class="button__next"><span class="icon-chevron-right"></span></button>'
   });
 
+  $('.news__item').slick({
+    arrows: true,
+    prevArrow: '<button type="button" class="button__prev button__prev_news"><span class="icon-chevron-left"></span></button>',
+    nextArrow: '<button type="button" class="button__next button__next_news"><span class="icon-chevron-right"></span></button>'
+  });
+
 });
