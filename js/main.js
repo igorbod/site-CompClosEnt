0b0034abc5f18bc9b51a5e440c9679fe1d6c7d64
Igor <igorbod.webdev@gmail.com>
2020-02-14 15:46:33 +0700

    made responsive for header section and main slider, also made hamburger menu for small screen devices

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
   
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+      menuItem = document.querySelectorAll('.menu__link'),
+      hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+      });
+    });
+  });
+  
   $('.slider__inner').slick({
     arrows: true,
     prevArrow: '<button type="button" class="button__prev"><span class="icon-chevron-left"></span></button>',
