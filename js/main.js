4d005306618759ed9f95d11ff4317113bffc2242
Igor <igorbod.webdev@gmail.com>
2020-02-15 14:18:03 +0700

    add scroll to top button with JS

--- js/main.js
+++ js/main.js
@@ -30,4 +30,32 @@ $(document).ready(function () {
     nextArrow: '<button type="button" class="button__next button__next_news"><span class="icon-chevron-right"></span></button>'
   });
 
+  
+  // Scroll To Top Button
+  const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+  window.addEventListener("scroll", scrollFunction);
+
+  function scrollFunction() {
+
+    if (window.pageYOffset > 300) { //Show our button when user scroll down 300px
+      // btnScrollToTop.style.display = "block";
+      btnScrollToTop.style.opacity = 1;
+
+    } else {                        //Hide button
+      // btnScrollToTop.style.display = "none";
+      btnScrollToTop.style.opacity = 0;
+    }
+  }
+
+  btnScrollToTop.addEventListener('click', function () {
+    // window.scrollTo(0, 0); //Simple use without animation
+
+    window.scrollTo({  //Simple use with smoth scroll
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  });
+
 });
